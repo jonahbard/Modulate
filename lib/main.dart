311a965e40b516ef74e556bedd9798bca599114c
Sanath Nair <sanathnair09@gmail.com>
2021-01-15 22:42:48 -0800

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:modulate_vsc/screens/home.dart';
 
-void main() {
-  
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
