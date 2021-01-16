d37ebd18076241ea4d9ce0d0782fcba98e53b2b6
Jonah <jonahbard@gmail.com>
2021-01-16 14:40:57 -0800

    made like half of create_track

--- lib/screens/create_track.dart
+++ lib/screens/create_track.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:modulate_vsc/src/module.dart';
 
 class CreateTrack extends StatefulWidget {
   @override
@@ -8,12 +7,56 @@ class CreateTrack extends StatefulWidget {
 }
 
 class _CreateTrackState extends State<CreateTrack> {
-  
 
+  List<Module> _moduleList;
+
+
+  Widget _buildModuleList() {
+    return new ListView.builder(
+      itemBuilder: (context, index){
+        if (index < _trackList.length){
+          return _buildModuleInList(_moduleList[index].name, index);
+        }
+      }
+    );
+  }
+  Widget buildModuleInList(String name, String info, int index){
+    return Card(
+      child: ListTile(
+        title: Text(name), //replace with function that gets track title
+        subtitle: Text("info goes here"), //replace with function that calculates tasks completed
+        onTap: () => _openTrack(index)
+        )
+      ),
+    );
+  }
+  void _addModule(String name, String info){
+    setState(() {
+      _moduleList.add(Module(name, info));
+    }
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Create track screen"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Track"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {
+              Navigator.pop(context);
+            }
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _showEditableModule,
+      ),
+      body: ListView.builder(
+
+      )
     );
   }
 }
