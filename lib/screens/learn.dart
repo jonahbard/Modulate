89a99947ce5771888fd4c50b020a8e7e6349192b
Jonah <jonahbard@gmail.com>
2021-01-15 22:47:52 -0800

    added stateless learn widget

--- lib/screens/learn.dart
+++ lib/screens/learn.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class LearnPage extends StatefulWidget {
+  @override
+  _LearnPageState createState() => _LearnPageState();
+}
+
+class _LearnPageState extends State<LearnPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Learn"),
+      ),
+    );
+  }
+}
