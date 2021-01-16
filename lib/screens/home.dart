bd7288685718c03bce3f9f7ca0eb6b6de6e638fd
Sanath Nair <sanathnair09@gmail.com>
2021-01-15 22:14:50 -0800

    home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
-
-
 }
 
 class _HomeState extends State<Home> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("home screen"),
+        bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        iconSize: 20,
+        backgroundColor: Colors.grey[200],
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: "Learn",
+            icon: Icon(Icons.library_books),
+          ),
+          BottomNavigationBarItem(
+              label: "Explore", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(
+            label: "Profile",
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
     );
   }
 }
