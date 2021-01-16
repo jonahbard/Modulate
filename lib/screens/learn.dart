d9afaeabdf7a04c31bd74f7ac1d67b8c466d6bc1
Jonah <jonahbard@gmail.com>
2021-01-16 09:59:02 -0800

    added a couple of methods

--- lib/screens/learn.dart
+++ lib/screens/learn.dart
@@ -6,6 +6,26 @@ class LearnPage extends StatefulWidget {
 }
 
 class _LearnPageState extends State<LearnPage> {
+  Widget _buildTrackList() {
+    return new ListView.builder(
+      itemBuilder: (context, index){
+        if (index < _trackList.length){
+          return _buildTrackInList(_trackList[index].trackName, index);
+          }
+        }
+      }
+      )
+  }
+  Widget buildTrackInList(String trackName, int index){
+    return Card(
+      child: ListTile(
+        title: Text("Track 1"), //replace with function that gets track title
+        subtitle: Text("3/5 modules completed"), //replace with function that calculates tasks completed
+        onTap: () => _openTrack(index)
+        )
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
