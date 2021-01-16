41535a20cbece515ca2253bb7dc1df9219fc082b
Sanath Nair <sanathnair09@gmail.com>
2021-01-15 22:21:58 -0800

    stuff

--- lib/screens/track_home.dart
+++ lib/screens/track_home.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:modulate_vsc/src/track.dart';
 
 class TrackHome extends StatelessWidget {
-  
+  final Track _track = Track();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: ListView(
+        children: [],
+      ),
     );
   }
 }
