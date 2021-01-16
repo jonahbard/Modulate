3e558db00d2bdd6d822bc93ce456bb6aa23d8197
Jonah Bard <jonahbard@gmail.com>
2021-01-16 15:49:28 -0800

    finished basic non-database version of create_track

--- lib/screens/create_track.dart
+++ lib/screens/create_track.dart
@@ -7,56 +7,90 @@ class CreateTrack extends StatefulWidget {
 }
 
 class _CreateTrackState extends State<CreateTrack> {
-
   List<Module> _moduleList;
-
+  void _pushAddEditableModule() {
+    String editableName = "";
+    String editableContent = "";
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+          appBar: AppBar(
+            title: Text('New Module'),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.check),
+                  onPressed: () {
+                    _addModule(editableName, editableContent);
+                    Navigator.pop(context);
+                  })
+            ],
+          ),
+          body: Column(
+            children: <Widget>[
+              TextField(
+                  autofocus: true,
+                  onSubmitted: (val) {
+                    editableName = val;
+                    //Navigator.pop(context);
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Enter module name...",
+                      contentPadding: const EdgeInsets.all(16.0))),
+              TextField(
+                  autofocus: true,
+                  onSubmitted: (val) {
+                    editableContent = val;
+                    //Navigator.pop(context);
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Enter module info...",
+                      contentPadding: const EdgeInsets.all(16.0)))
+            ],
+          ));
+    }));
+  }
 
   Widget _buildModuleList() {
-    return new ListView.builder(
-      itemBuilder: (context, index){
-        if (index < _trackList.length){
-          return _buildModuleInList(_moduleList[index].name, index);
-        }
+    return new ListView.builder(itemBuilder: (context, index) {
+      if (index < _moduleList.length) {
+        return _buildModuleInList(
+            _moduleList[index].name, _moduleList[index].content, index);
       }
-    );
+    });
   }
-  Widget buildModuleInList(String name, String info, int index){
+
+  Widget _buildModuleInList(String name, String info, int index) {
     return Card(
-      child: ListTile(
-        title: Text(name), //replace with function that gets track title
-        subtitle: Text("info goes here"), //replace with function that calculates tasks completed
-        onTap: () => _openTrack(index)
-        )
-      ),
-    );
+        child: ListTile(
+            title: Text(name), //replace with function that gets track title
+            subtitle: Text(
+                "info goes here"), //replace with function that calculates tasks completed
+            onTap: () => _pushAddEditableModule()));
   }
-  void _addModule(String name, String info){
+
+  void _addModule(String name, String info) {
     setState(() {
       _moduleList.add(Module(name, info));
-    }
-    );
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Create Track"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.check),
-            onPressed: () {
-              Navigator.pop(context);
-            }
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _showEditableModule,
-      ),
-      body: ListView.builder(
-
-      )
-    );
+        appBar: AppBar(
+          title: Text("Create Track"),
+          leading: IconButton(icon: Icon(Icons.close)),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {
+                  Navigator.pop(context);
+                })
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _pushAddEditableModule,
+        ),
+        body: _buildModuleList());
   }
 }
