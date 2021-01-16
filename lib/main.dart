a551956535e1ac2f27a74aae4bbc2a18e8135de6
Sanath Nair <sanathnair09@gmail.com>
2021-01-15 22:40:50 -0800

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:modulate_vsc/screens/home.dart';
 
 void main() {
+  
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
