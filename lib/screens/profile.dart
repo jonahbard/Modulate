871b49f9f118eec95c443b574bd1abe27f2a6009
Jonah Bard <jonahbard@gmail.com>
2021-01-16 16:22:03 -0800

    added basic profile page with user icon

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -8,8 +8,30 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Page"),
+    return Column(
+      children: <Widget>[
+        Container(
+          child: Image(image: AssetImage('assets/pink_profile_masked.png')),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("John Smith"),
+          ),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Modulator since Jan 15, 2021"),
+          ),
+        ),
+        Row(
+          children: [
+            Container(),
+            Container(),
+          ],
+        )
+      ],
     );
   }
 }
