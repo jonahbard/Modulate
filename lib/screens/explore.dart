051c8ee22aca99090c6d21cedec0809c24b15b05
Jonah <jonahbard@gmail.com>
2021-01-17 00:39:55 -0800

    created basic exploreCard widget, without images

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -6,122 +6,124 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
+  Widget exploreCard(String trackName, int modules) {
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 5,
+        color: Colors.grey[300],
+        child: Column(children: [
+          Container(
+            width: 150,
+            height: 100,
+            //color: Colors.blueGrey[300],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+              color: Colors.white,
+            ),
+          ),
+          Container(
+              width: 150,
+              height: 50,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Text(trackName),
+                    Text("$modules Modules",
+                        style: TextStyle(color: Colors.grey))
+                  ],
+                ),
+              ))
+        ]));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        child: Column(children: <Widget>[
-          SizedBox(
-            width: double.infinity,
-            child: Container(
-              padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
-              child: Text(
-                "Based on your recent activity...",
-                textAlign: TextAlign.left,
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              child: Container(
+                color: Colors.transparent,
+                padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                child: Text(
+                  "Based on your recent activity...",
+                  textAlign: TextAlign.left,
+                ),
               ),
             ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Card(
-                    child: Container(
-                        width: 150,
-                        height: 150,
-                        color: Colors.redAccent,
-                        child: Column(
-                          children: [
-                            Container(
-                                width: 150, height: 50, color: Colors.black),
-                          ],
-                        ))),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.blueGrey[300])),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.greenAccent))
-              ],
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  exploreCard("Introduction to AI", 35),
+                  exploreCard("How to spel", 17),
+                  exploreCard("Gramar is cool", 21),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: Container(
-                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
-                  child: Text("Popular Tracks", textAlign: TextAlign.left))),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.pinkAccent)),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.purple[300])),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.blueAccent))
-              ],
+            SizedBox(
+              height: 10,
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: Container(
-                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
-                  child: Text("Popular Tracks", textAlign: TextAlign.left))),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.yellow[700])),
-                Card(
-                    child: Container(
-                        width: 150,
-                        height: 150,
-                        color: Colors.deepOrange[300])),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.greenAccent))
-              ],
+            SizedBox(
+                width: double.infinity,
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                    color: Colors.transparent,
+                    child: Text("Popular Tracks", textAlign: TextAlign.left))),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  exploreCard("Introduction to AI", 35),
+                  exploreCard("How to spel", 17),
+                  exploreCard("Gramar is cool", 21),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: Container(
-                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
-                  child: Text("Popular Tracks", textAlign: TextAlign.left))),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.redAccent)),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.purple[300])),
-                Card(
-                    child: Container(
-                        width: 150, height: 150, color: Colors.greenAccent))
-              ],
+            SizedBox(
+              height: 10,
             ),
-          )
-        ]),
-      ),
-    );
+            SizedBox(
+                width: double.infinity,
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                    child:
+                        Text("Try something new", textAlign: TextAlign.left))),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  exploreCard("Introduction to AI", 35),
+                  exploreCard("How to spel", 17),
+                  exploreCard("Gramar is cool", 21),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+                width: double.infinity,
+                child: Container(
+                    padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                    child: Text("Recently added", textAlign: TextAlign.left))),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  exploreCard("Introduction to AI", 35),
+                  exploreCard("How to spel", 17),
+                  exploreCard("Gramar is cool", 21),
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 }
