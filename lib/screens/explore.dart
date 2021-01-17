7bed4cd71b9c2a1a4c7ea04d419cb8cf3e18037f
Jonah <jonahbard@gmail.com>
2021-01-16 22:50:05 -0800

    added colorful and scrollable explore cards

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -10,54 +10,118 @@ class _ExplorePageState extends State<ExplorePage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(children: <Widget>[
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-            child: Text(
-              "Based on your recent activity...",
-              textAlign: TextAlign.left,
-            ),
-          ),
-        ),
-        Row(
-          children: [
-            Card(
-                child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.redAccent,
-                    child: Column(
-                      children: [
-                        Container(width: 100, height: 50, color: Colors.black),
-                      ],
-                    ))),
-            Card(
-                child: Container(
-                    width: 100, height: 100, color: Colors.deepOrange[300])),
-            Card(
-                child: Container(
-                    width: 100, height: 100, color: Colors.greenAccent))
-          ],
-        ),
-        SizedBox(
+      child: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          SizedBox(
             width: double.infinity,
             child: Container(
-                child: Text("Popular Tracks", textAlign: TextAlign.left))),
-        Row(
-          children: [
-            Card(
-                child: Container(
-                    width: 100, height: 100, color: Colors.redAccent)),
-            Card(
-                child: Container(
-                    width: 100, height: 100, color: Colors.deepOrange[300])),
-            Card(
-                child: Container(
-                    width: 100, height: 100, color: Colors.greenAccent))
-          ],
-        )
-      ]),
+              padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+              child: Text(
+                "Based on your recent activity...",
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Card(
+                    child: Container(
+                        width: 150,
+                        height: 150,
+                        color: Colors.redAccent,
+                        child: Column(
+                          children: [
+                            Container(
+                                width: 150, height: 50, color: Colors.black),
+                          ],
+                        ))),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.blueGrey[300])),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.greenAccent))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+              width: double.infinity,
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                  child: Text("Popular Tracks", textAlign: TextAlign.left))),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.pinkAccent)),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.purple[300])),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.blueAccent))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+              width: double.infinity,
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                  child: Text("Popular Tracks", textAlign: TextAlign.left))),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.yellow[700])),
+                Card(
+                    child: Container(
+                        width: 150,
+                        height: 150,
+                        color: Colors.deepOrange[300])),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.greenAccent))
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+              width: double.infinity,
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                  child: Text("Popular Tracks", textAlign: TextAlign.left))),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.redAccent)),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.purple[300])),
+                Card(
+                    child: Container(
+                        width: 150, height: 150, color: Colors.greenAccent))
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
